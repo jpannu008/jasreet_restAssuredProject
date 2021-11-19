115daf0eeb49b51516668e52af11cfce2d363f8e
jpannu008 <jasreet.kaur008@gmail.com>
2021-11-19 20:59:33 +0530

    Committing pom.xml and sample test class

---
+++ src/test/java/testDemoRun/HelloWorld.java
@@ -0,0 +1,9 @@
+package testDemoRun;
+import io.restassured.RestAssured.*;
+
+public class HelloWorld {
+ 
+    public void testHelloWorld(){
+        System.out.println("Hello World!!!");
+    }
+}
